9600c0f71a022be38edbeaa7518b0673bacc0bf9
SatoShun <shun.sato1@gmail.com>
2020-05-14 10:19:52 +0000

    chip

--- app/src/main/java/com/github/satoshun/example/horizontalchipbind/HorizontalChipBindActivity.kt
+++ app/src/main/java/com/github/satoshun/example/horizontalchipbind/HorizontalChipBindActivity.kt
@@ -4,19 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.*
+import com.github.satoshun.example.R
 import com.github.satoshun.example.databinding.ChipBinding
-import com.github.satoshun.example.databinding.HorizontalChipBindlActBinding
+import com.github.satoshun.example.databinding.HorizontalChipBindActBinding
+import com.google.android.material.chip.Chip
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import com.xwray.groupie.Item
 
 class HorizontalChipBindActivity : AppCompatActivity() {
-  private lateinit var binding: HorizontalChipBindlActBinding
+  private lateinit var binding: HorizontalChipBindActBinding
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    binding = HorizontalChipBindlActBinding.inflate(layoutInflater)
+    binding = HorizontalChipBindActBinding.inflate(layoutInflater)
     setContentView(binding.root)
 
     // ok
@@ -25,6 +27,61 @@ class HorizontalChipBindActivity : AppCompatActivity() {
       adapter = SampleAdapter().apply {
         submitList((0..1000).map { it.toString() })
       }
+    }
+
+    // ok
+    with(binding.recycler2) {
+      layoutManager = StaggeredGridLayoutManager(
+        2,
+        StaggeredGridLayoutManager.HORIZONTAL
+      )
+      adapter = SampleAdapter().apply {
+        submitList((0..1000).map { it.toString() })
+      }
+    }
+
+    // ok
+    with(binding.recycler3) {
+      layoutManager = StaggeredGridLayoutManager(
+        2,
+        StaggeredGridLayoutManager.HORIZONTAL
+      )
+      adapter = SampleAdapter().apply {
+        submitList((0..1000).map { it.toString() })
+      }
+    }
+
+    // ok
+    with(binding.recycler4) {
+      layoutManager = StaggeredGridLayoutManager(
+        2,
+        StaggeredGridLayoutManager.HORIZONTAL
+      )
+      adapter = SampleAdapter2().apply {
+        update((0..1000).map {
+          SampleItem(it.toString())
+        })
+      }
+    }
+
+    // ok
+    with(binding.recycler5) {
+      val myAdapter = SampleAdapter2().apply {
+        update((0..1000).map {
+          SampleItem(it.toString())
+        })
+      }
+      adapter = myAdapter
+      layoutManager = StaggeredGridLayoutManager(
+        2,
+        StaggeredGridLayoutManager.HORIZONTAL
+      )
+      setPadding(
+        20,
+        20,
+        20,
+        20
+      )
     }
   }
 }
@@ -51,5 +108,18 @@ private class SampleAdapter : ListAdapter<String, RecyclerView.ViewHolder>(
   override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
     val binding = ChipBinding.bind(holder.itemView)
     binding.chip.text = position.toString()
+  }
+}
+
+private class SampleAdapter2 : GroupAdapter<GroupieViewHolder>()
+
+private class SampleItem(
+  val item: String
+) : Item<GroupieViewHolder>() {
+  override fun getLayout(): Int = R.layout.chip
+
+  override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+    val chip = viewHolder.itemView as Chip
+    chip.text = item
   }
 }
